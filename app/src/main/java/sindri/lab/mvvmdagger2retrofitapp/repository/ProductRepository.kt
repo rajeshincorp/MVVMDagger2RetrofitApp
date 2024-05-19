04c6ee22650806a5840ee785bd48092cc86deaed
RajeshKumar <rajeshincorp@gmail.com>
2024-05-19 14:42:56 +0530

    ProductRepository class added

---
+++ app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/repository/ProductRepository.kt
@@ -0,0 +1,24 @@
+package sindri.lab.mvvmdagger2retrofitapp.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Response
+import sindri.lab.mvvmdagger2retrofitapp.model.Product
+import sindri.lab.mvvmdagger2retrofitapp.retrofit.FakeAPI
+import javax.inject.Inject
+
+class ProductRepository @Inject constructor(private val fakeAPI: FakeAPI) {
+
+    private var _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>>                    //exposed to outer world for access
+
+    get() = _products
+
+    suspend fun getProducts() {
+        val result = fakeAPI.getProducts()
+        if (result.isSuccessful && result.body() != null) {
+            _products.postValue(result.body())
+        }
+    }
+
+}
