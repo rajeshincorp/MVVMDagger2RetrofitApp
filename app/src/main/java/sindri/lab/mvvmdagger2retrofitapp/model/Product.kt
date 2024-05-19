241a23bb85a4dc549b52dda95eee6b86745893f9
RajeshKumar <rajeshincorp@gmail.com>
2024-05-19 13:37:03 +0530

    data class and base url added

---
+++ app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/model/Product.kt
@@ -0,0 +1,10 @@
+package sindri.lab.mvvmdagger2retrofitapp.model
+
+data class Product(
+    val category: String,
+    val description: String,
+    val id: Int,
+    val image: String,
+    val price: Double,
+    val title: String
+)
---
+++ app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package sindri.lab.mvvmdagger2retrofitapp.utils
+
+object Constants {
+    const val BASE_URL = "https://fakestoreapi.com/"
+}
