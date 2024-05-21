1adbc7d29669b823a551dc12b26b8996ec1860b4
RajeshKumar <rajeshincorp@gmail.com>
2024-05-21 15:45:38 +0530

    recorrecting Product class for Entity class

--- app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/model/Product.kt
+++ app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/model/Product.kt
@@ -1,8 +1,13 @@
 package sindri.lab.mvvmdagger2retrofitapp.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Product(
     val category: String,
     val description: String,
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val image: String,
     val price: Double,
