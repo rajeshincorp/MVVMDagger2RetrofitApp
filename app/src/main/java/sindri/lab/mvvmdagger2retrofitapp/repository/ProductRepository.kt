6f00b4936db0f267996dc1965845e1cae5d425a4
RajeshKumar <rajeshincorp@gmail.com>
2024-05-21 16:21:52 +0530

    saving data into room db

--- app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/repository/ProductRepository.kt
+++ app/src/main/java/sindri/lab/mvvmdagger2retrofitapp/repository/ProductRepository.kt
@@ -3,11 +3,13 @@ package sindri.lab.mvvmdagger2retrofitapp.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import retrofit2.Response
+import sindri.lab.mvvmdagger2retrofitapp.db.FakerDB
 import sindri.lab.mvvmdagger2retrofitapp.model.Product
 import sindri.lab.mvvmdagger2retrofitapp.retrofit.FakeAPI
 import javax.inject.Inject
 
-class ProductRepository @Inject constructor(private val fakeAPI: FakeAPI) {
+class ProductRepository @Inject constructor(private val fakeAPI: FakeAPI,
+    private val fakerDB: FakerDB) {
 
     private var _products = MutableLiveData<List<Product>>()
     val products: LiveData<List<Product>>                    //exposed to outer world for access
@@ -16,6 +18,7 @@ class ProductRepository @Inject constructor(private val fakeAPI: FakeAPI) {
     suspend fun getProducts() {
         val result = fakeAPI.getProducts()
         if (result.isSuccessful && result.body() != null) {
+            fakerDB.getFakerDAO().addProduct(result.body()!!)
             _products.postValue(result.body())
         }
     }
